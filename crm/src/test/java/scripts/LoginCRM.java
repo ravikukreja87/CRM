f7c1f867917638e2d000f21b3245d4b95bf81c20
ravi8 <ravi8@RAVI>
2017-09-10 13:59:58 +0530

    Correcting assertion exp result

--- crm/src/test/java/scripts/LoginCRM.java
+++ crm/src/test/java/scripts/LoginCRM.java
@@ -21,7 +21,7 @@ public class LoginCRM {
 		driver.findElement(By.id("loginbtn")).click();
 		WebElement label = driver.findElement(By.id("ctl00_lblcompanyname"));
 		String actualResult = label.getText();
-		String expectedResult = "Test";
+		String expectedResult = "Techcanvass";
 		Assert.assertEquals(actualResult, expectedResult, "Does not match");
 	}
 
