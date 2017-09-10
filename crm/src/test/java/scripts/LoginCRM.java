f1130a95185880a1d18e3ee6e8cd10aa55d89080
ravi8 <ravi8@RAVI>
2017-09-10 13:26:31 +0530

    Adding Login - Success Test Adding chromedriver.exe Initial POM and TestNG XML

---
+++ crm/src/test/java/scripts/LoginCRM.java
@@ -0,0 +1,40 @@
+package scripts;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+
+public class LoginCRM {
+	WebDriver driver;
+
+	@Test
+	public void login() {
+		driver.get("http://crm.techcanvass.co.in/");
+		driver.findElement(By.id("btnlogin")).click();
+		driver.findElement(By.id("txtuname")).sendKeys("techcanvassuser@techcanvass.co.in");
+		driver.findElement(By.id("txtpwd")).sendKeys("user1234");
+		driver.findElement(By.id("loginbtn")).click();
+		WebElement label = driver.findElement(By.id("ctl00_lblcompanyname"));
+		String actualResult = label.getText();
+		String expectedResult = "Techcanvass";
+		Assert.assertEquals(actualResult, expectedResult, "Does not match");
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+
+		System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		driver.quit();
+	}
+
+}
